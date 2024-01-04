e803e82bb3926194503e1a92b928cd04fad3b375
linyi <969610745@qq.com>
2024-01-05 01:42:39 +0800

    first commit

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:lackment/my-blog.git main:gh-pages
+git push -f git@github.com:lackment/my-vitepress-blog.git master:gh-pages
 
 cd -
