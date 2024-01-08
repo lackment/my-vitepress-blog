004a175d806062f9df58150ed9ee8705786d2331
yilin <yilin@shein.com>
2024-01-08 20:52:21 +0800

    feat: live2d插件接入

---
+++ docs/.vitepress/theme/components/live2d/index.ts
@@ -0,0 +1,9 @@
+const loadLive2d = () => {
+  localStorage.setItem('modelId', '2')
+  const live2D = document.createElement('script')
+  live2D.src =
+    'https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/autoload.js'
+  document.body.appendChild(live2D)
+}
+
+export default loadLive2d
