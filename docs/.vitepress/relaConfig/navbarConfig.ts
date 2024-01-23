6446c1cad26854f4dbb08b76b8ce2822f5e13025
yilin <yilin@shein.com>
2024-01-23 22:27:49 +0800

    feat: 博客内容修改

--- docs/.vitepress/relaConfig/navbarConfig.ts
+++ docs/.vitepress/relaConfig/navbarConfig.ts
@@ -6,6 +6,10 @@ export const nav: DefaultTheme.NavItem[] = [
       {
         text: 'vue详解',
         link: '/vue/vue.md' // 对应docs/vue下的idnex.md文件
+      },
+      {
+        text: '有趣的技术尝试',
+        link: '/interestingTechnologies/technologies.md' 
       }
     ]
   },
